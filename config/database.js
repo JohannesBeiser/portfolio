d637ec91abf557a91bac34a8e4076b1f40384645
JohannesBeiser <beiserjohannes@googlemail.com>
2018-10-31 23:12:12 +0100

    customizing base Project

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
+  database: 'mongodb://johannes:8ung8ung@ds147033.mlab.com:47033/heroku_bf6ftm4f',   //prod
   //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'yoursecret'
 }
