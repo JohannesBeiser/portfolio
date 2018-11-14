900e9b770bf334e2a159134ec7813c5060a8e8cc
JohannesBeiser <beiserjohannes@googlemail.com>
2018-11-14 21:27:41 +0100

    Uses the right ConnectionString now

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   //database: 'mongodb://johannes:8ung8ung@ds147033.mlab.com:47033/heroku_bf6ftm4f',   //prod
 
 
-  //atabase: process.env.MONGODB_URI, //TODO: Make this work --> reinstall mLab on Heroku
-  database: 'mongodb://localhost:27017/portfolio',    //dev
+  database: process.env.MONGODB_URI, //TODO: Make this work --> reinstall mLab on Heroku
+  //database: 'mongodb://localhost:27017/portfolio',    //dev
   secret: 'yoursecret'
 }
