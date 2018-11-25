9cec76d2c400a3a00f6bdadc7d4e976b47ede8a1
JohannesBeiser <beiserjohannes@googlemail.com>
2018-11-25 02:13:15 +0100

    Main Images are now being served as .webp instead of png

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
 
-  database: process.env.MONGODB_URI,
-  //database: 'mongodb://localhost:27017/portfolio',    //dev
+  //database: process.env.MONGODB_URI,
+  database: 'mongodb://localhost:27017/portfolio',    //dev
   secret: 'yoursecret'
 }
