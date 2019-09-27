fbcf7a29419438277118a3d7b230360659be49a5
Johannes Beiser <beiser@w11k.de>
2019-09-27 15:52:14 +0200

    Updated Skills & Experience

--- angular-src/src/app/components/about/about.component.ts
+++ angular-src/src/app/components/about/about.component.ts
@@ -25,19 +25,19 @@ export class AboutComponent implements OnInit {
       {
         iconPath: "../../../assets/icons/html.svg",
         technology: "HTML, CSS/LESS",
-        skillLevel: "90%"
+        skillLevel: "95%"
       },{
         iconPath: "../../../assets/icons/javascript.png",
         technology: "Javascript, Typescript",
-        skillLevel: "70%"
+        skillLevel: "80%"
       },{
         iconPath: "../../../assets/icons/node.png",
         technology: "Node.js + Express",
-        skillLevel: "60%"
+        skillLevel: "80%"
       },{
         iconPath: "../../../assets/icons/angular.svg",
         technology: "Angular",
-        skillLevel: "45%"
+        skillLevel: "85%"
       },{
         iconPath: "../../../assets/icons/mongodb.png",
         technology: "MongoDB",
@@ -45,11 +45,11 @@ export class AboutComponent implements OnInit {
       },{
         iconPath: "../../../assets/icons/vue.png",
         technology: "Vue",
-        skillLevel: "30%"
+        skillLevel: "20%"
       },{
         iconPath: "../../../assets/icons/git.png",
         technology: "Git",
-        skillLevel: "80%"
+        skillLevel: "90%"
       },{
         iconPath: "../../../assets/icons/jquery.png",
         technology: "JQuery",
