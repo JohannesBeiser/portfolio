b01233f60d0013b69a1ffefa5c4dc789e709b3b1
Beiser Johannes <jb191@hdm-stuttgart.de>
2020-11-28 23:02:24 +0100

    Work screen added

--- angular-src/src/app/components/work/work.component.ts
+++ angular-src/src/app/components/work/work.component.ts
@@ -10,7 +10,33 @@ export class WorkComponent implements OnInit {
 
   constructor() { }
 
+
+  activeExpenseImage: number = 0;
+  activeTravelImage: number = 0;
+
   ngOnInit() {
+  }
+
+  updateActiveExpenseImage(value: number){
+    const numberOfExpenseImages = 5;
+    if(this.activeExpenseImage == 0 && value <0){
+      this.activeExpenseImage = numberOfExpenseImages -1
+    }else if(this.activeExpenseImage == numberOfExpenseImages-1 && value >0){
+      this.activeExpenseImage = 0
+    }else{
+      this.activeExpenseImage+= value;
+    }
+  }
+
+  updateActiveTravelImage(value: number){
+    const numberOfExpenseImages = 6;
+    if(this.activeTravelImage == 0 && value <0){
+      this.activeTravelImage = numberOfExpenseImages -1
+    }else if(this.activeTravelImage == numberOfExpenseImages-1 && value >0){
+      this.activeTravelImage = 0
+    }else{
+      this.activeTravelImage+= value;
+    }
   }
 
 }
